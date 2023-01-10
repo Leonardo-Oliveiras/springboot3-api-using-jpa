a7f11ca728dba85193b1845f76f93bab0fc08097
80847835 <leonardo.s@compasso.com.br>
2023-01-10 16:43:46 -0300

    Service layer, component registration

---
+++ src/main/java/com/leonardooliveira/courseproject/services/UserService.java
@@ -0,0 +1,26 @@
+package com.leonardooliveira.courseproject.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.leonardooliveira.courseproject.entities.User;
+import com.leonardooliveira.courseproject.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
